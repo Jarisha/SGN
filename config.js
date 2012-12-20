19b57e5b110ad7afb9ff9025546d5ff4264acaaf
root <root@jerry-PowerEdge-T420.(none)>
2012-12-20 14:42:09 -0800

    updated FB app data & secret in config file for dev & tony

--- config.js
+++ config.js
@@ -6,11 +6,11 @@ exports.redis_host = '10.0.1.49'
 //Dev
 exports.dev_host = '10.0.1.49';
 exports.dev_port = '3000';
-exports.dev_Fb_ID = "313249415456863";
-exports.dev_Fb_Secret = "cabb1ae80788c824000f0745a98575e4";
+exports.dev_Fb_ID = "387033471379256";
+exports.dev_Fb_Secret = "5481010ba4013aa2b1629de5f08b3a6b";
 
 //Tony
 exports.tony_host = '10.0.1.29';
 exports.tony_port = '3001';
-exports.tony_Fb_ID = "177752685682496";
-exports.tony_Fb_Secret = "bcd13f474bae8217643ece20c68cd3a4";
+exports.tony_Fb_ID = "313249415456863";
+exports.tony_Fb_Secret = "cabb1ae80788c824000f0745a98575e4";
