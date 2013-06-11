8c3937dfd39d1043d04cd8ede9c0a7022584ec8f
Coleman Foley <colemanfoley@gmail.com>
2013-06-11 11:50:05 -0700

    Made follower notification modal presentable.

--- config.js
+++ config.js
@@ -1,17 +1,20 @@
+//making this a var as our fucking IP address changes whenever it wants
+var dev_ip = '10.1.10.18';
+
 /* Shared Config */
 exports.CDN = 'http://1d0cd4fe9d04a622c153-67a9c5f20077a0cbea40c22fb907467d.r89.cf1.rackcdn.com';
 
 /* Dev Config */
 //Riak
-exports.dev_db_host = '10.1.10.38';
+exports.dev_db_host = dev_ip;
 exports.dev_db_port = 8100;
 //exports.dev_ssl_path = 'C:/node/Q_SSL/';
 
 exports.dev_temp_path = 'C:/Users/Tony/AppData/Local/Temp/';
 exports.dev_log_path = 'C:/node/Q_LOG/';
 exports.dev_host = 'localhost';
-exports.dev_redis_host = '10.1.10.38';
-exports.dev_nodeflake_host = '10.1.10.38';
+exports.dev_redis_host = dev_ip;
+exports.dev_nodeflake_host = dev_ip;
 exports.dev_port = '80';
 
 /* Staging Config */
@@ -33,7 +36,7 @@ exports.production_db_port = 8098;
 exports.production_ssl_path = '/var/lib/quyay/';
 exports.production_temp_path = '/tmp';
 exports.production_log_path = '/var/log/quyay/';
-exports.production_host = "www.quyay.com";
+exports.production_host = 'www.quyay.com';
 exports.production_redis_host = 'redis1.quyay.com';
 exports.production_nodeflake_host = '127.0.0.1';
 exports.production_port = '80';
